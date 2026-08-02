b6e13b556e62c01da6062fc39fb478f54663ea73
Debora <deboratanowe.aguiar@gmail.com>
2026-08-02 18:08:56 -0300

    adding maven

--- cakeshop/src/main/java/com/deboraayumi/model/Product.java
+++ cakeshop/src/main/java/com/deboraayumi/model/Product.java
@@ -1,4 +1,4 @@
-package model;
+package com.deboraayumi.model;
 
 public class Product {
 
--- cakeshop/src/main/java/com/deboraayumi/model/ShoppingCart.java
+++ cakeshop/src/main/java/com/deboraayumi/model/ShoppingCart.java
@@ -1,4 +1,4 @@
-package model;
+package com.deboraayumi.model;
 
 import java.util.ArrayList;
 import java.util.List;
