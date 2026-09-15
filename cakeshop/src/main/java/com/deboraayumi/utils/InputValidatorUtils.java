2b4d790b41f31eb1c5fd53a4ee57bc29dba60d30
Debora <deboratanowe.aguiar@gmail.com>
2026-09-15 09:14:01 -0300

    feature: split the function to attend to types of input: >0 or >=0

--- cakeshop/src/main/java/com/deboraayumi/utils/InputValidatorUtils.java
+++ cakeshop/src/main/java/com/deboraayumi/utils/InputValidatorUtils.java
@@ -5,16 +5,40 @@ import java.util.Scanner;
 public class InputValidatorUtils {
     
     Scanner scanner = new Scanner(System.in);
+    int number = 0;
 
-    public int isInputAnInt(){
+    public int getPositiveInt(){
 
-        if(!scanner.hasNextInt()){
-            System.out.println("Invalid value. Please insert a positive number.");
-            scanner.next();
-            return -1;
+        while(true){
+            if(!scanner.hasNextInt()){
+                System.out.println("Invalid value. Please insert a positive number.");
+                scanner.next();
+            }
+
+            number = scanner.nextInt();
+
+            if(number<0){
+                System.out.println("Invalid value. Please insert a positive number.");
+            }
+
+            break;
         }
-
-        return scanner.nextInt();
-
+        return number;
+        
     }
+
+    public int getNonNegativeInt(){
+
+        while(true){
+            if(!scanner.hasNextInt()){
+                System.out.println("Invalid value. Please insert a positive number.");
+                scanner.next();
+            }
+
+            break;
+        }
+        return number;
+        
+    }
+
 }
